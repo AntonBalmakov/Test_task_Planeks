ce501609622701e3f27af2f2f169bacf8d3ddf1c
Balmakov Anton <balmakov.anton@gmail.com>
2020-12-31 10:20:48 +0300

    commit

--- mysite/task/admin.py
+++ mysite/task/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import FirstUserModel
 
-# Register your models here.
+admin.site.register(FirstUserModel)
--- mysite/task/models.py
+++ mysite/task/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class FirstUserModel(models.Model):
+    TEXT = (
+        ('Предложения', '1'),
+        ('Предложения', '2'),
+        ('Предложения', '3'),
+        ('Предложения', '4'),
+    )
+
+    first_name = models.CharField(max_length=32, verbose_name="first name")
+    last_name = models.CharField(max_length=32, verbose_name="last name")
+    Job = models.CharField(max_length=32, verbose_name="Job")
+    email = models.EmailField(verbose_name='email', null=True)
+    domain_name = models.CharField(max_length=32, verbose_name="domain name")
+    phone_number = models.CharField(max_length=20, verbose_name="phone number")
+    company_name = models.CharField(max_length=32, verbose_name="company name")
+    order = models.CharField(max_length=32, choices=TEXT, verbose_name="order")
+    address = models.CharField(max_length=32, verbose_name="address")
+    date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
